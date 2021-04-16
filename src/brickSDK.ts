f5d67defe0626ae01569f53ad2b0b6a7bec983ea
archimonde12 <>
2021-04-16 18:22:23 +0700

    1.1.7

--- src/brickSDK.ts
+++ src/brickSDK.ts
@@ -286,10 +286,10 @@ class BrickSDK {
     public async debitSubAcc(uuid: string, username: string, amount: number, asset: 'eur' | 'trx' | 'usdt_trc20', action: 'bonus' | 'debit' | 'credit' | 'convert' | 'transfer' | 'cancel'): Promise<Transaction> {
         try {
             // if (!username) throw new Error(BMErrorCode.PARAM_MISSING)
-            if (!asset) throw new Error(BMErrorCode.PARAM_MISSING)
+            // if (!asset) throw new Error(BMErrorCode.PARAM_MISSING)
             // if (!usernameRegex.test(username)) throw new Error(BMErrorCode.PARAM_INVALID)
-            if (!(typeof amount === `number`) || amount < 0) throw new Error(BMErrorCode.PARAM_INVALID)
-            if (!AssetType.includes(asset)) throw new Error(BMErrorCode.PARAM_INVALID)
+            // if (!(typeof amount === `number`) || amount < 0) throw new Error(BMErrorCode.PARAM_INVALID)
+            // if (!AssetType.includes(asset)) throw new Error(BMErrorCode.PARAM_INVALID)
             // if (!Object.values(ActionType).includes(action)) throw new Error(ErrorCode.PARAM_INVALID)
             let res = await this.GetData(BMMethodType.debitSub, { uuid, username, amount, asset, action }) as Transaction
             return res
@@ -308,11 +308,11 @@ class BrickSDK {
      */
     public async creditSubAcc(uuid: string, username: string, amount: number, asset: 'eur' | 'trx' | 'usdt_trc20', action: 'bonus' | 'debit' | 'credit' | 'transfer' | 'convert' | 'cancel'): Promise<Transaction> {
         try {
-            if (!username) throw new Error(BMErrorCode.PARAM_MISSING)
-            if (!asset) throw new Error(BMErrorCode.PARAM_MISSING)
+            // if (!username) throw new Error(BMErrorCode.PARAM_MISSING)
+            // if (!asset) throw new Error(BMErrorCode.PARAM_MISSING)
             // if (!usernameRegex.test(username)) throw new Error(BMErrorCode.PARAM_INVALID)
-            if (!(typeof amount === `number`) || amount < 0) throw new Error(BMErrorCode.PARAM_INVALID)
-            if (!AssetType.includes(asset)) throw new Error(BMErrorCode.PARAM_INVALID)
+            // if (!(typeof amount === `number`) || amount < 0) throw new Error(BMErrorCode.PARAM_INVALID)
+            // if (!AssetType.includes(asset)) throw new Error(BMErrorCode.PARAM_INVALID)
             // if (!Object.values(ActionType).includes(action)) throw new Error(ErrorCode.PARAM_INVALID)
             let res = await this.GetData(BMMethodType.creditSub, { uuid, username, amount, asset, action }) as Transaction
             return res
@@ -331,11 +331,11 @@ class BrickSDK {
      */
     public async SubAccRequestWithDraw(uuid: string, username: string, amount: number, asset: 'eur' | 'trx' | 'usdt_trc20', receiver: string): Promise<Transaction> {
         try {
-            if (!username) throw new Error(BMErrorCode.PARAM_MISSING)
-            if (!asset) throw new Error(BMErrorCode.PARAM_MISSING)
-            if (!usernameRegex.test(username)) throw new Error(BMErrorCode.PARAM_INVALID)
-            if (!(typeof amount === `number`) || amount < 0) throw new Error(BMErrorCode.PARAM_INVALID)
-            if (!AssetType.includes(asset)) throw new Error(BMErrorCode.PARAM_INVALID)
+            // if (!username) throw new Error(BMErrorCode.PARAM_MISSING)
+            // if (!asset) throw new Error(BMErrorCode.PARAM_MISSING)
+            // if (!usernameRegex.test(username)) throw new Error(BMErrorCode.PARAM_INVALID)
+            // if (!(typeof amount === `number`) || amount < 0) throw new Error(BMErrorCode.PARAM_INVALID)
+            // if (!AssetType.includes(asset)) throw new Error(BMErrorCode.PARAM_INVALID)
             let res = await this.GetData(BMMethodType.requestWithdrawSub, { uuid, username, amount, asset, receiver }) as Transaction
             return res
         } catch (e) {
